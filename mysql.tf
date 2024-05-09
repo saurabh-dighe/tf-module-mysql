ae804385a836c98dab7bd44d56ec480fb5d4ad48
Saurabh Dighe <sd.saurabhdighe@gmail.com>
2024-05-09 12:44:53 +0530

    auto pushhh

--- mysql.tf
+++ mysql.tf
@@ -7,7 +7,7 @@ resource "aws_db_instance" "mysql" {
   instance_class          = "db.t3.micro"
   username                = "admin1"
   password                = "RoboShop1"
-  parameter_group_name    = "mysql.mysql5.7"
+  parameter_group_name    = aws_db_parameter_group.mysql.name
   skip_final_snapshot     = true
   db_subnet_group_name    = aws_db_subnet_group.mysql.name
   vpc_security_group_ids  = [aws_security_group.allow_mysql.id]
