1d285b68ad71ddb085cc239d887ad65478805662
Saurabh Dighe <sd.saurabhdighe@gmail.com>
2024-05-09 12:07:39 +0530

    auto pushhh

--- mysql.tf
+++ mysql.tf
@@ -1,6 +1,6 @@
 
 #Provisions RDS instance
-resource "aws_db_instance" "default" {
+resource "aws_db_instance" "mysql" {
   allocated_storage       = 10
   db_name                 = "roboshop-${var.ENV}-mysql"
   engine                  = "mysql"
@@ -8,7 +8,7 @@ resource "aws_db_instance" "default" {
   instance_class          = "db.t3.micro"
   username                = "admin"
   password                = "RoboShop1"
-  parameter_group_name    = "default.mysql5.7"
+  parameter_group_name    = "mysql.mysql5.7"
   skip_final_snapshot     = true
   db_subnet_group_name    = aws_db_subnet_group.mysql.name
   vpc_security_group_ids  = [aws_security_group.allow_mysql.id]
