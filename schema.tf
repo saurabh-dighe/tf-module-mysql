2244337e93a98a99dd6f94a4df0fcd19640ee722
Saurabh Dighe <sd.saurabhdighe@gmail.com>
2024-05-12 00:03:36 +0530

    auto pushhh

--- schema.tf
+++ schema.tf
@@ -1,13 +1,29 @@
+# resource "null_resource" "schema" {
+#     depends_on = [aws_db_instance.mysql]
+#   # Establishes connection to be used by all
+#   provisioner "local-exec" {
+#     command = <<EOF
+#     cd /tmp
+#     curl -s -L -o /tmp/mysql.zip "https://github.com/stans-robot-project/mysql/archive/main.zip"
+#     unzip mysql.zip
+#     cd mysql-main
+#     mysql -h ${aws_db_instance.mysql.address} -uadmin1 -pRoboShop1 <shipping.sql
+#     EOF
+#   }
+# }
+
 resource "null_resource" "schema" {
+
+    # This make sures that this null_resource will only be executed post the creation of the RDS only    
     depends_on = [aws_db_instance.mysql]
-  # Establishes connection to be used by all
-  provisioner "local-exec" {
-    command = <<EOF
-    cd /tmp
-    curl -s -L -o /tmp/mysql.zip "https://github.com/stans-robot-project/mysql/archive/main.zip"
-    unzip mysql.zip
-    cd mysql-main
-    mysql -h ${aws_db_instance.mysql.address} -uadmin1 -pRoboShop1 <shipping.sql
-    EOF
-  }
+
+      provisioner "local-exec" {
+        command = <<EOF
+            cd /tmp
+            curl -s -L -o /tmp/mysql.zip "https://github.com/stans-robot-project/mysql/archive/main.zip"
+            unzip -o mysql.zip
+            cd mysql-main
+            mysql -h ${aws_db_instance.mysql.address}  -uadmin1 -pRoboShop1 < shipping.sql
+        EOF
+    }
 }
