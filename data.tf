1cf7241f93eb8b4da9b5ccfc776cbe1ba772ce08
Saurabh Dighe <sd.saurabhdighe@gmail.com>
2024-05-11 21:52:21 +0530

    auto pushhh

--- data.tf
+++ data.tf
@@ -6,3 +6,12 @@ data "terraform_remote_state" "vpc" {
     region = "us-east-1"
   }
 }
+
+data "aws_secretsmanager_secret" "roboshop_secrets" {
+  name      = "roboshop/secrets"            
+}
+
+#Extracting the sectrets
+data "aws_secretsmanager_secret_version" "secret_version" {
+  secret_id = data.aws_secretsmanager_secret.roboshop_secrets.id
+}
