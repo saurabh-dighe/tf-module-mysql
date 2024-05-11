dfb3a233258a16919af90eaac57a66e04f1fe5c3
Saurabh Dighe <sd.saurabhdighe@gmail.com>
2024-05-11 23:53:22 +0530

    auto pushhh

--- schema.tf
+++ schema.tf
@@ -7,7 +7,7 @@ resource "null_resource" "schema" {
     curl -s -L -o /tmp/mysql.zip "https://github.com/stans-robot-project/mysql/archive/main.zip"
     unzip mysql.zip
     cd mysql-main
-    mysql -h ${aws_db_instance.mysql.address} -ucentos -pRoboShop1 <shipping.sql
+    mysql -h ${aws_db_instance.mysql.address} -uadmin1 -pRoboShop1 <shipping.sql
     EOF
   }
 }
